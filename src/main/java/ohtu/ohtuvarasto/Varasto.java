fe6886c3a3c5b9443487f87998ab3eb7bfaf8cd8
kriskrok <25787775+kriskrok@users.noreply.github.com>
2019-11-10 22:00:45 +0200

    more pattern testing

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -32,6 +32,14 @@ public class Varasto {
         }
     }
 
+    public void generateError() {
+        for( int i=0; i<1; i++ ) {
+            for( int j=0; i<j; j++ ) {
+                System.out.println("virhe");
+            }
+        }
+    }
+
     // --- ottavat aksessorit eli getterit: ---
     public double getSaldo() {
         return saldo;
